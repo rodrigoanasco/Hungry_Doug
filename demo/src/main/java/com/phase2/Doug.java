59386eedbd8f3d049a99899ee7dbdc3df39f1043
Josh <jjk24@sfu.ca>
2024-10-27 03:18:18 -0700

    Implemented a gamepanel class which consists of the game window in which the game will be played in, and Doug as a movable character in this window

--- demo/src/main/java/com/phase2/Doug.java
+++ demo/src/main/java/com/phase2/Doug.java
@@ -6,22 +6,43 @@ public class Doug {
     private int health;
     private int score;
 
+    private final int speed = 5; // speed of movement
+
     public Doug() {
-        this.positionX = 0;
-        this.positionY = 0;
+        this.positionX = 250;
+        this.positionY = 250;
         this.score = 0;
         this.health = 100;
     }
 
+    // move method to update Doug's position based on direction
     public void move(String direction) {
-        // implement move logic here
+        switch (direction) {
+            case "UP":
+                positionY -= speed;
+                break;
+            case "DOWN":
+                positionY += speed;
+                break;
+            case "LEFT":
+                positionX -= speed;
+                break;
+            case "RIGHT":
+                positionX += speed;
+                break;
+        }
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
     }
 
     public int getScore() {
         return score;
     }
-
-    
-
 
 }
--- demo/src/main/java/com/phase2/Main.java
+++ demo/src/main/java/com/phase2/Main.java
@@ -1,7 +1,21 @@
 package com.phase2;
 
+import javax.swing.JFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame window = new JFrame(); // library for a new window
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // able to close the window
+        window.setResizable(false); // cannot resize window
+        window.setTitle("Hungry Doug");
+
+        GamePanel gamePanel = new GamePanel ();
+        gamePanel.startGameThread();
+        window.add(gamePanel);
+
+        window.pack(); // too see panel
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
