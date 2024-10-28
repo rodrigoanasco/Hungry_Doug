3334959a9a4f85898918116a895a1f762ac29d5e
Tim Yeh <tya74@sfu.ca>
2024-10-27 18:27:37 -0700

    enemy classes based on gameObject

---
+++ demo/src/main/java/com/phase2/Cat.java
@@ -0,0 +1,16 @@
+package main.java;
+
+import java.awt.Graphics;
+
+
+public class Cat extends GameObject {
+    public Cat(int x, int y, ID id) {
+        super(x,y,id);
+    }
+    public void tick() {
+
+    }
+    public void render(Graphics g) {
+
+    }
+}
