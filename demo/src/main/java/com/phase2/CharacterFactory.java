53dc118a735c8edc64fa61a388d5a63d4a1b7af9
Sarah Harb <sha310@sfu.ca>
2024-10-27 18:35:45 -0700

    merged sarah branch

--- demo/src/main/java/com/phase2/CharacterFactory.java
+++ demo/src/main/java/com/phase2/CharacterFactory.java
@@ -2,7 +2,7 @@ package com.phase2;
 
 public class CharacterFactory {
 
-    public static Doug createDoug() {
-        return new Doug();
-    }
+    // public static Doug createDoug() {
+    //     return new Doug();
+    // }
 }
--- demo/src/main/java/com/phase2/Game.java
+++ demo/src/main/java/com/phase2/Game.java
@@ -55,7 +55,7 @@ public class Game extends Canvas implements Runnable {
         double ns = 1000000000 / amountOfTicks;
         double delta = 0;
         long timer = System.currentTimeMillis();
-        int frames = 0;
+        // int frames = 0;
         while (running) {
             long now = System.nanoTime();
             delta += (now - lastTime) / ns;
@@ -67,11 +67,11 @@ public class Game extends Canvas implements Runnable {
             if (running) {
                 render();
             }
-            frames++;
+            // frames++;
             if (System.currentTimeMillis() - timer > 1000) {
                 timer += 1000;
                 // System.out.println("FPS: " + frames);
-                frames = 0;
+                // frames = 0;
             }
         }
         stop();
