40b6a93489d21178202398fd5168d905e7a7df59
Sarah Harb <sha310@sfu.ca>
2024-10-27 21:36:47 -0700

    "modified class structure, added MovingEnemy class"

--- demo/src/main/java/com/phase2/Onion.java
+++ demo/src/main/java/com/phase2/Onion.java
@@ -3,28 +3,13 @@ package com.phase2;
 import java.awt.Graphics;
 
 
-<<<<<<< HEAD
-
-=======
-/**
- * Class for the non-moving enemy type Onion
- * @param x the x position the objects appears in
- * @param y the y position the objects appears in
- * @param id the type of object the object should be treated as
-*/
->>>>>>> enemy
-public class Onion extends GameObject {
-    public Onion(int x, int y, ID id) {
-        super(x,y,id);
 public class Onion extends Punishment {
     public Onion(int x, int y) {
         super(x,y, PunishmentType.ONION, 10);
     }
-<<<<<<< HEAD
     public void tick() {
 
     }
-=======
     /**
      * What the object should do on each tick
      */
@@ -34,7 +19,6 @@ public class Onion extends Punishment {
     /**
      * How the object should look like
      */
->>>>>>> enemy
     public void render(Graphics g) {
 
     }
