95eac67758160d263adf7fd892e7b40793fe778f
Tim Yeh <tya74@sfu.ca>
2024-11-04 18:15:55 -0800

    added comments

--- demo/src/main/java/com/phase2/DebugBuild.java
+++ demo/src/main/java/com/phase2/DebugBuild.java
@@ -1,5 +1,10 @@
 package com.phase2;
 
+
+/**
+ * A test build to run the game
+ * - currently has hitboxes
+ */
 public class DebugBuild {
     public static void main(String[] args) {
         Game test = new Game();
