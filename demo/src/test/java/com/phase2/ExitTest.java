51cd7146fc4c854bdb88eeeade438a055c35fcb9
Tim Yeh <tya74@sfu.ca>
2024-11-22 17:42:43 -0800

    ExitTest

---
+++ demo/src/test/java/com/phase2/ExitTest.java
@@ -0,0 +1,49 @@
+package com.phase2;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+public class ExitTest {
+    Exit exit;
+    Score score;
+
+    @Before
+    public void setUp() {
+        Score.boneScore=0;
+        Score.boneTotal=0;
+        exit = new Exit(0,0);
+    }
+
+    @Test
+    public void testHidden() {
+        Bone bone = new Bone(0,10);
+        exit.tick();
+        assertTrue(exit.getHiddenStatus());
+    }
+
+    @Test
+    public void testActivated() {
+        
+        Bone bone = new Bone(0,10);
+        Score.boneScore++;
+        exit.tick();
+
+        assertFalse(exit.getHiddenStatus());
+    }
+
+    @Test
+    public void testActivatedNoBone() {
+        
+        exit.tick();
+        assertFalse(exit.getHiddenStatus());
+    }
+
+
+
+
+}
